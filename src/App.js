9d34ecec91f6730472d293247944c944f33f5cd1
shahram <shahramkhaleh2001@gmail.com>
2022-08-05 23:17:45 -0700

    created NavBar Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import SideBar from "./components/SideBar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <SideBar />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SideBar.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import { FaMicrophone } from "react-icons/fa";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineMessage } from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
+import { MdOutlineComputer } from "react-icons/md";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { BsUpload } from "react-icons/bs";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { BsArrowRightCircleFill } from "react-icons/bs";
+
+
+
+const SideBar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={` ${open ? "w-36 gap-4 shadow-md bg-purple-300 shadow-white " :"w-16 gap-20 bg-white"} duration-200 h-screen p-4 pt-8 flex flex-col items-center  justify-start relative`}>
+      <div
+        onClick={() => setOpen(!open)}
+        className={`${open ? "rotate-180" : "rotate-0"} duration-500 absolute top-2 cursor-pointer -right-3 text-purple-700 text-2xl`}
+      >
+        <BsArrowRightCircleFill />
+      </div>
+      <div className="w-8 flex justify-center items-center h-8 rounded-lg bg-purple-800">
+        <FaMicrophone className="text-white text-xl" />
+      </div>
+      <nav className="">
+        <ul className={`${open ? "gap-2":"gap-6"} flex items-center flex-col `}>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold`}
+            >
+              <AiOutlineHome className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >home</span>}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold relative`}
+            >
+              <AiOutlineMessage className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >message</span>}
+              <span className="w-1 rounded-full absolute h-1 bg-green-600 flex p-1 top-0 right-0"></span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold relative`}
+            >
+              <BsTelephone className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >contacts</span>}
+              <span className="w-1 rounded-full absolute h-1 bg-green-600 flex p-1 top-0 right-0"></span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold`}
+            >
+              <MdOutlineComputer className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >shared</span>}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold`}
+            >
+              <BsFillPeopleFill className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >your friend</span>}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold`}
+            >
+              <BsUpload className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >upload</span>}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold relative`}
+            >
+              <AiOutlineCalendar className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >calender</span>}
+              <span className="w-1 rounded-full absolute h-1 bg-green-600 flex p-1 top-0 right-0"></span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/"
+              className={`p-2 gap-1 flex-col  text-gray-600 flex justify-start items-center font-bold`}
+            >
+              <FiSettings className={`${open? "text-lg text-purple-800" : "text-2xl text-gray-700"}`} />
+              {open && <span className="text-purple-800" >setting</span>}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default SideBar;
